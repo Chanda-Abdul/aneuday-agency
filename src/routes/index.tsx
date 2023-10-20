80bca46ffcf2496661051563940cbd917998669b
Chanda Abdul <56906759+Chanda-Abdul@users.noreply.github.com>
2023-10-19 17:25:32 -0700

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useVisibleTask$ } from '@builder.io/qwik';
+import { component$} from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import Hero from '~/components/hero/hero';
 import About from '~/components/About/about';
@@ -6,58 +6,18 @@ import Company from '~/components/Company/company';
 import Events from '~/components/Events/events';
 import Projects from '~/components/Projects/projects';
 
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import LocomotiveScroll from 'locomotive-scroll';
 
 
 export default component$(() => {
-  useVisibleTask$(() => {
-    /*  Only runs in the client, for GSAP animations */
-gsap.registerPlugin(ScrollTrigger);
-// const scroll = new LocomotiveScroll();
-// gsap.from('#scroll', {  backgroundColor: 'red',})    
-// gsap.fromTo('#trigger', {},{
-//   backgroundColor: 'rgb(150,200,75)',
-//  opacity: 1,
-//       scrollTrigger: {
-//         // pin: true,
-//         id: 'parent',
-//         trigger: '#trigger',
-//         start: 'top center',
-//         scroller: '#scroll',
-//         markers: true,
-//         scrub: true,
-//       },
-//     });
-
-    // ScrollTrigger.refresh();
-  });
-
-  function initSmoothScroll() {
-    /* smooth scroll/locomotive scroll */
-  }
-
-  initSmoothScroll();
-
-  return (
+ 
+ return (
     <>
-      <div id='scroll'>
-        <div id='trigger'>
+      <div>
           <Hero />
-        </div>
-        <div id='trigger'>
           <About />
-        </div>
-        <div id='trigger'>
           <Projects />
-        </div>
-        <div id='trigger'>
           <Events />
-        </div>
-        <div id='trigger'>
           <Company />
-        </div>
       </div>
     </>
   );
